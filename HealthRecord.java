d9773475ebf5cd595da28ced8171e58750646667
Kasid1n <kasidinkhamfoo@gmail.com>
2025-12-26 15:46:29 +0700

    Create HealthRecord.java

---
+++ HealthRecord.java
@@ -0,0 +1,60 @@
+public class HealthRecord {
+    private int height;
+    private static final int MIN_PERMITTED_HEIGHT = 50;
+    private static final int MAX_PERMITTED_HEIGHT = 175;
+    private static final int DEFAULT_HEIGHT = 0;
+    private static int tallestHeight =50;
+    private static int shortestHeight =175;
+    private static int counter =0;
+    private static double averageHeight = 0;
+
+    public HealthRecord(int height){
+        setHeight(height);
+        averageHeight = ((averageHeight*counter) + height) / (counter+1);
+        counter++;
+    }
+
+
+    public void setHeight(int height){
+        if(height >= MIN_PERMITTED_HEIGHT && height <= MAX_PERMITTED_HEIGHT) {
+            this.height = height;
+        }
+        else {
+            this.height = DEFAULT_HEIGHT;
+
+        }
+
+        if(this.height > tallestHeight)
+            tallestHeight = this.height;
+        if(this.height < shortestHeight )
+            shortestHeight = this.height;
+
+    }
+
+
+
+    public static int getTallestHeight(){
+        return HealthRecord.tallestHeight;
+    }
+
+    public static int getShortestHeight(){
+        return HealthRecord.shortestHeight;
+    }
+
+    public int getHeight(){
+        return this.height;
+    }
+
+    public static double getAverageHeight(){
+        return HealthRecord.averageHeight;
+    }
+
+    public void displayDetails() {
+        System.out.println("Height (cm): " + getHeight());
+    }
+    public static void displayClassDetails() {
+        System.out.println("The tallest height (cm): " + getTallestHeight());
+        System.out.println("The shortest height (cm): " + getShortestHeight());
+        System.out.println("The average Height (cm): "+ getAverageHeight());
+    }
+}
